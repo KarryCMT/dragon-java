36dd30cf199d1bf85cc3bc2b9a0957f21e646903
coco <1570606465@qq.com>
2024-02-18 22:04:49 +0800

    修改：日期格式化

--- dragon-api/src/main/java/com/coco/dragon/resp/BaseResp.java
+++ dragon-api/src/main/java/com/coco/dragon/resp/BaseResp.java
@@ -21,7 +21,7 @@ public class BaseResp {
     /**
      * 创建时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss" ,timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" ,timezone = "GMT+8")
     private Date createTime;
 
     /**
@@ -33,7 +33,7 @@ public class BaseResp {
     /**
      * 更新时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss" ,timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" ,timezone = "GMT+8")
     private Date updateTime;
 
     /**
